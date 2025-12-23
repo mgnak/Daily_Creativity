efcfbdb78af1ac85d154202cb7cbf05df8f2a106
Mgna <meghanamgnak@gmail.com>
2025-12-23 20:19:36 +0530

    File updated

--- programs/src/main/java/org/example/CharacterCount.java
+++ programs/src/main/java/org/example/CharacterCount.java
@@ -5,14 +5,13 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class CharacterCount {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner (System.in);
-            String str =sc.next().toLowerCase(); //"programming";
-            Map<Character, Integer> count = new HashMap<>();
-            for (char c : str.toCharArray()) {
-                count.put(c, count.getOrDefault(c, 0) + 1);
-            }
-            System.out.println(count); // {p=1, r=2, o=1, g=2, a=1, m=2, i=1, n=1}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String str =sc.next().toLowerCase(); //"programming";
+        Map<Character, Integer> count = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
         }
-
+        System.out.println(count); // {p=1, r=2, o=1, g=2, a=1, m=2, i=1, n=1}
+    }
 }

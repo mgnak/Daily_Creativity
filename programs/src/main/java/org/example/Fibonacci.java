dcf4b93b485468274e628db6f64cbcc172235ac7
Mgna <meghanamgnak@gmail.com>
2026-01-04 21:27:00 +0530

    File updated

--- programs/src/main/java/org/example/Fibonacci.java
+++ programs/src/main/java/org/example/Fibonacci.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            for (int i = 0; i < n; i++) {
-                System.out.print(fibonacci(i) + " ");
-            }
-            System.out.println();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
         }
-
-        public static int fibonacci(int n) {
-            if (n <= 1) {
-                return n;
-            }
-            return fibonacci(n - 1) + fibonacci(n - 2);
-        }
+        System.out.println();
     }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
